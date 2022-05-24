6a38e98271fcf0f29090751c7bbe3de1b7e0c183
Scraba Anani Alexandru <alexanani18@gmail.com>
2022-05-24 22:17:54 +0300

    Versiunea 0.6

--- js/anim.js
+++ js/anim.js
@@ -1,4 +1,20 @@
+//Primul efect linie carusel
 $(document).ready(function(){
-  $('.fadeAnim').hide(),
-  $('.fadeAnim').slideDown(3000)
+  $('.slideDown1').hide(),
+  $('.slideDown1').slideDown(1500)
+})
+//Al doilea efect linie carusel
+$(document).ready(function(){
+  $('.slideDown2').hide(),
+  $('.slideDown2').slideDown(2000)
+})
+//Al treilea efect linie carusel
+$(document).ready(function(){
+  $('.slideDown3').hide(),
+  $('.slideDown3').slideDown(2500)
+})
+//Al patrulea efect linie carusel
+$(document).ready(function(){
+  $('.slideDown4').hide(),
+  $('.slideDown4').slideDown(3000)
 })
